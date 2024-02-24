9529f24962b8a8f68cdd3c9d963d66c48e5fdca9
Egor-Zharskiy <egor.zharskiy@mail.ru>
2024-02-24 12:36:05 +0300

    parser now is much more prettier

--- lab_3/parser.py
+++ lab_3/parser.py
@@ -6,34 +6,25 @@ from collections import Counter
 import string
 
 
-def plot_char_frequency(url, target_word):
-    # Отправляем GET-запрос к указанному URL
-    response = requests.get(url)
+def plot_char_frequency(html_response, target_word):
+    # response = requests.get(url)
 
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        # Используем BeautifulSoup для парсинга HTML-кода страницы
-        soup = BeautifulSoup(response.text, 'html.parser')
+    if html_response.status_code == 200:
 
-        # Извлекаем текст из HTML-кода
+        soup = BeautifulSoup(html_response.text, 'html.parser')
+
         text = soup.get_text()
-
-        # Удаляем знаки препинания и лишние пробелы
         translator = str.maketrans('', '', string.punctuation)
         text = text.translate(translator).lower()
 
-        # Считаем частоту каждой буквы в тексте
         char_counter = Counter(text)
 
-        # Извлекаем данные для построения гистограммы
         labels, values = zip(*char_counter.items())
 
-        # Сортируем данные
         sorted_indices = sorted(range(len(labels)), key=lambda k: labels[k])
         labels = [labels[i] for i in sorted_indices]
         values = [values[i] for i in sorted_indices]
 
-        # Построение гистограммы
         plt.bar(labels, values)
         plt.xlabel('Буквы')
         plt.ylabel('Частота встречаемости')
@@ -41,28 +32,17 @@ def plot_char_frequency(url, target_word):
         plt.show()
 
     else:
-        print(f"Ошибка при запросе: {response.status_code}")
+        print(f"Ошибка при запросе: {html_response.status_code}")
 
 
-def plot_word_length_histogram(url):
-    # Отправляем GET-запрос к указанному URL
-    response = requests.get(url)
+def plot_word_length_histogram(html_response):
+    if html_response.status_code == 200:
+        soup = BeautifulSoup(html_response.text, 'html.parser')
 
-    # Проверяем успешность запроса
-    if response.status_code == 200:
-        # Используем BeautifulSoup для парсинга HTML-кода страницы
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Извлекаем текст из HTML-кода
         text = soup.get_text()
-
-        # Разбиваем текст на слова
         words = text.split()
-
-        # Считаем длину каждого слова
         word_lengths = [len(word) for word in words]
 
-        # Построение гистограммы
         plt.hist(word_lengths, bins=range(1, max(word_lengths) + 2), edgecolor='black')
         plt.xlabel('Длина слова')
         plt.ylabel('Частота встречаемости')
@@ -72,37 +52,32 @@ def plot_word_length_histogram(url):
         plt.show()
 
     else:
-        print(f"Ошибка при запросе: {response.status_code}")
+        print(f"Ошибка при запросе: {html_response.status_code}")
 
 
-def get_html(url):
-    response = requests.get(url)
-    # print(response.text)
-    return response.text
+def get_response(page_url):
+    html_response = requests.get(page_url)
+    return html_response
 
 
-def find_all_links(url):
-    soup = BeautifulSoup(get_html(url), 'html.parser')
+def find_all_links(page_url):
+    soup = BeautifulSoup(get_response(page_url).text, 'html.parser')
     links = []
     for a_tag in soup.find_all("a"):
         links.append(a_tag)
 
-    # print(links)
-    print(len(links))
-    # links = soup.find_all('href')
-    # print(links, 'links')
+    return len(links)
 
 
-def find_words(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+def find_words(html_response):
+    soup = BeautifulSoup(html_response.text, 'html.parser')
     text = soup.get_text()
     words = [word.lower() for word in re.findall(r'\b\w+\b', text)]
     return words
 
 
-def count_word(url, word):
-    words = find_words(url)
+def count_word(html_response, word):
+    words = find_words(html_response)
     print(words)
     count = words.count(word)
     for el in words:
@@ -112,13 +87,13 @@ def count_word(url, word):
 
 
 url = input('Введите url-адрес анализируемой страницы: ')
-
-find_all_links(url)
-find_words(url)
+response = get_response(url)
+print(f'Количество всех ссылок на странице: {find_all_links(url)}')
+find_words(response)
 
 symbol = input('введите символ или слово для проверки частоты его встречаемости: ')
-print(f'Число встречи "{symbol}": ', count_word(url, symbol))
-plot_char_frequency(url, symbol)
+print(f'Число встречи "{symbol}": ', count_word(response, symbol))
+plot_char_frequency(response, symbol)
 
-plot_word_length_histogram(url)
+plot_word_length_histogram(response)
 # https://baskino.org/
